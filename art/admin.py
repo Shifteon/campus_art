7abba795d288838f872cd58919cfd6e941ba4ff8
Joshua Dean <dea16006@byui.edu>
2021-05-21 13:21:26 -0600

    Database, Settings, Admin update
    Updated the models.py with the Building_Name, Category, and Artwork classes. Updated the settings.py to include our app and a media_url and media_root. Updated the admin.py to include the created models.

--- art/admin.py
+++ art/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+#Added import for models to the admin
+from .models import Building_Name,Category,Artwork
 # Register your models here.
+
+class Building_NameAdmin(admin.ModelAdmin):
+    pass
+
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+class ArtworkAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Building_Name, Building_NameAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Artwork, ArtworkAdmin)
