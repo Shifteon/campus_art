1c14104aaa9e4673c3054a0a2a41ccceabfd46ff
Joshua Dean <dea16006@byui.edu>
2021-05-21 13:48:21 -0600

    Updated the Views
    Updated the views,py to contain requests for views for all the art, views for artwork by category, and views for artwork by building name.

--- art/views.py
+++ art/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
+from. models import Building_Name,Category,Artwork
 
 # Create your views here.
+
+# An index card version of the artwork
+def art_index(request):
+    artwork = Artwork.objects.all().order_by('-title')
+    context = {
+        "artwork": artwork,
+    }
+    return render(request, "art_index.html", context)
+
+# Artwork organized by building
+#Show blog posts in specific category
+def art_category(request, category):
+    artwork = Artwork.objects.filter(
+        categories__name__contains=category
+    ).order_by(
+        '-title'
+    )
+    context = {
+        "category": category,
+        "artwork": artwork
+    }
+    return render(request, "art_category.html", context)
+
+# Artwork organized by category
+def art_building(request, building):
+    artwork = Artwork.objects.filter(
+        building__name__contains=building
+    ).order_by(
+        '-title'
+    )
+    context = {
+        "building": building,
+        "artwork": artwork
+    }
+    return render(request, "art_building.html", context)
