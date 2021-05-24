9423f9e500379f2c15b42f08c6861311998ef9a6
Shifteon <bpwyatt04@gmail.com>
2021-05-24 16:02:30 -0600

    Added base and building templates. Added urls/view

--- art/views.py
+++ art/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def building(request):
+    numbers = range(1, 20)
+    context = {"numbers": numbers}
+    return render(request, 'art/building.html', context)
