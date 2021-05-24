d34cd99d61620a876e1700cf7abee462d32735ab
Shifteon <bpwyatt04@gmail.com>
2021-05-24 17:04:58 -0600

    Improved building page

--- art/views.py
+++ art/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def building(request):
-    numbers = range(1, 20)
+    numbers = range(0, 20)
     context = {"numbers": numbers}
     return render(request, 'art/building.html', context)
