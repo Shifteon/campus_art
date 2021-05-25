70483b5ef125a54b5fc2d9d41810dd2023978744
Joshua Dean <dea16006@byui.edu>
2021-05-25 09:38:58 -0600

    Merge branch 'main' of https://github.com/Shifteon/campus_art

--- art/admin.py
+++ art/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-<<<<<<< HEAD
 
 # Register your models here.
-=======
+
 #Added import for models to the admin
 from .models import Building_Name,Category,Artwork
 # Register your models here.
@@ -19,4 +18,4 @@ class ArtworkAdmin(admin.ModelAdmin):
 admin.site.register(Building_Name, Building_NameAdmin)
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Artwork, ArtworkAdmin)
->>>>>>> e99d7ae0937b439555fef499770cc18157befbf4
+
