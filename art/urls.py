1b081a0136eedc8ffb5d9ea5ea2b88494cf04a22
Shifteon <bpwyatt04@gmail.com>
2021-05-25 14:31:35 -0600

    Added trailing slash in url

--- art/urls.py
+++ art/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.art_index, name="art_index"),
-    path("category/<category>", views.art_category, name="art_category"),
-    path("building/<building>", views.art_building, name="art_building"),
+    path("category/<category>/", views.art_category, name="art_category"),
+    path("building/<building>/", views.art_building, name="art_building"),
 ]
