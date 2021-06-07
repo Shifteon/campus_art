0200a9d525a4b86eb1e5d4a577de5eac7ed6f23a
Sami Wang <82911626+syw24@users.noreply.github.com>
2021-06-07 13:04:38 -0600

    map page

--- art/urls.py
+++ art/urls.py
@@ -6,9 +6,6 @@ urlpatterns = [
     path("", ArtworkListView.as_view(), name="art_index"),
     path("category/<category>", ArtCategoryView.as_view(), name="art_category"),
     path("building/<building>", ArtBuildingView.as_view(), name="art_building"),
-
-    # For the map
-    #path("map/"), CampusMapView.as_view(), name="art_map"),
     path("map/", views.map, name="art_map"),
 
 ]
