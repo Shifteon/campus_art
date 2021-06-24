1fe146aad20ba92ed5d7aaef4efc3ba900cfe358
Sami Wang <82911626+syw24@users.noreply.github.com>
2021-06-24 12:04:04 -0600

    try to insert the picture as a marker

--- art/urls.py
+++ art/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from .views import ArtworkListView, ArtBuildingCategoryView, BuildingListView 
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
     path("", views.map, name="art_map"),
     path("<building>/<category>/<floor>/", ArtBuildingCategoryView.as_view(), name="art_building_category"),
-    path("<building>/<category>/", ArtBuildingCategoryView.as_view(), name="art_building_category"), #
+    path("<building>/<category>/", ArtBuildingCategoryView.as_view(), name="art_building_category"), 
 ]
+
+# be able to import the media
+urlpatterns += staticfiles_urlpatterns()
